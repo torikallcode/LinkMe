2bd66425922bc6704516d6bf6f016504b946f83d
torikallcode <torikalcontent@gmail.com>
2025-02-12 15:38:22 +0800

    create component anouncement

---
+++ src/components/announcement/Announcement.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Announcement = () => {
+  return (
+    <div className="bg-emerald-600 px-4 py-3 shadow-md text-white fixed z-[9999] top-0 left-0 w-full">
+      <p className="text-center text-sm font-medium">
+        Untuk order langsung ke WA ya!!😊
+      </p>
+    </div>
+  )
+}
